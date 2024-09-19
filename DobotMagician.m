9e0ea7d59b617e774ca480f49ac494a5ca3539a7
Omar Malla <92977092+OmarMalla@users.noreply.github.com>
2024-09-19 16:04:46 +0530

    Update DobotMagician.m

--- DobotMagician.m
+++ DobotMagician.m
@@ -4,11 +4,10 @@ Dobot Simulation with Geometric approach and the DH Apporach based on the
 simplified model.
 
 Created as part of:
-Malla, O. and Shanmugavel, M. (2024),
-"Simplified model to study the kinematics of manipulators
-with parallelogram linkages", Industrial Robot,
-Vol. ahead-of-print No. ahead-of-print.
-https://doi.org/10.1108/IR-01-2024-0046 
+Malla, O. and Shanmugavel, M. (2024), 
+"Simplified model to study the kinematics of manipulators with parallelogram linkages", 
+Industrial Robot, Vol. 51 No. 5, pp. 704-714. 
+https://doi.org/10.1108/IR-01-2024-0046
 
 The code requires the use of Peter Corke's Robotics Toolbox for Matlab:
 https://petercorke.com/toolboxes/robotics-toolbox/
